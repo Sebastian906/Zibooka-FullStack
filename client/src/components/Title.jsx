e996700e2edc112b8b173f147dce6dfe1c6c82ea
Sebastian906 <elsebas1912@gmail.com>
2025-12-23 19:22:57 -0500

    Configuración del componente para mostrar las categorias de los libros

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const Title = () => {
+const Title = ({ 
+    title1, 
+    title2, 
+    titleStyles, 
+    title1Styles, 
+    paraStyles, 
+    para 
+}) => {
     return (
-        <div>Title</div>
+        <div className={titleStyles}>
+            <h3 className={`${title1Styles} h3 capitalize`}>
+                {title1} <span className='font-light underline'>{title2}</span>
+            </h3>
+            <p className={`${paraStyles} max-w-md mt-2`}>
+                {para 
+                    ? para 
+                    : "Discover books that spark curiosity deliver, quality and bring inspiration to your everyday reading."
+                }
+            </p>
+        </div>
     )
 }
 
