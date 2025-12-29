a4b38361f22706841a92b4734e50d9dde181642b
Sebastian906 <elsebas1912@gmail.com>
2025-12-28 23:38:58 -0500

    Configuración en la interfaz de la página para las ordenes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Cart from './pages/Cart'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import AddressForm from './pages/AddressForm'
+import MyOrders from './pages/MyOrders'
 
 const App = () => {
   return (
@@ -26,6 +27,7 @@ const App = () => {
         <Route path='/contact' element={<Contact />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/address-form' element={<AddressForm />} />
+        <Route path='/my-orders' element={<MyOrders />} />
       </Routes>
       <Footer />
     </main>
